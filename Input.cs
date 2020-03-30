af5533248462d644d97abfbd2bddcb9fba0a07aa
ParkGeunYeong <201645012@itc.ac.kr>
2020-03-30 14:09:59 +0900

    Branch Test

--- Input.cs
+++ Input.cs
@@ -7,7 +7,7 @@ public class Inputs : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Input-G Branch Test");
     }
 
     // Update is called once per frame
