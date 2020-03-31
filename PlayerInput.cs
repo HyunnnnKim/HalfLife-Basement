804f6adb24f26de43c79eb4f41d587ee81956949
ParkGeunYeong <201645012@itc.ac.kr>
2020-03-31 12:17:07 +0900

    XR Input Example

--- PlayerInput.cs
+++ PlayerInput.cs
@@ -1,23 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.XR;
+
+//https://docs.unity3d.com/Manual/xr_input.html
+//https://docs.unity3d.com/ScriptReference/XR.CommonUsages.html
 
 public class PlayerInput : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Input-G");
+        //4 ways for accessing input devices
+
+        //1. just get devices
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
+        }
+
+
+        //2. by Characteristics
+        var leftHandedControllers = new List<UnityEngine.XR.InputDevice>();
+        var desiredCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Left | UnityEngine.XR.InputDeviceCharacteristics.Controller;
+        UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(desiredCharacteristics, leftHandedControllers);
+
+        foreach (var device in leftHandedControllers)
+        {
+            Debug.Log(string.Format("Device name '{0}' has characteristics '{1}'", device.name, device.characteristics.ToString()));
+        }
+
+        //3. by role --> not work in oculus rift s
+        var gameControllers = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesWithRole(UnityEngine.XR.InputDeviceRole.GameController, gameControllers);
+
+        foreach (var device in gameControllers)
+        {
+            Debug.Log(string.Format("Device name '{0}' has role '{1}'", device.name, device.role.ToString()));
+        }
+
+        //4. by XR node
+        var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, leftHandDevices);
+
+        if (leftHandDevices.Count == 1)
+        {
+            UnityEngine.XR.InputDevice device = leftHandDevices[0];
+            Debug.Log(string.Format("Device name '{0}' with role '{1}'", device.name, device.role.ToString()));
+        }
+        else if (leftHandDevices.Count > 1)
+        {
+            Debug.Log("Found more than one left hand!");
+        }
+
     }
 
     private void FixedUpdate()
     {
-        Debug.Log("Input-G");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
 }
